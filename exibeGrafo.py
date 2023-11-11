5576f7bbc45a3768e70cb8d594e675badc0b91eb
Gabriel Calil <gabrielbcalil@gmail.com>
2023-11-11 17:12:14 -0300

    feat: add weight function

--- exibeGrafo.py
+++ exibeGrafo.py
@@ -2,3 +2,10 @@ def exibir_grafo(grafo):
     print("Seu Grafo: ")
     for vertice, vizinho in grafo.items():
         print(f"{vertice}: {', '.join(vizinho)}")
+
+def exibir_grafo_com_pesos(grafo):
+    for vertice, vizinhos in grafo.items():
+        print(f"{vertice}:", end=' ')
+        for vizinho, peso in vizinhos:
+            print(f"({vizinho} - {peso})", end=' ')
+        print()
