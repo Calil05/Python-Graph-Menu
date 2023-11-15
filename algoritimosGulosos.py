15d10d5065ba673090e7e7ae58ae7bb14c98dd3b
Gabriel Calil <gabrielbcalil@gmail.com>
2023-11-14 22:09:56 -0300

    feat: comment greedy algorithms - Kruskal

--- algoritimosGulosos.py
+++ algoritimosGulosos.py
@@ -3,7 +3,7 @@ from exibeGrafo import exibir_grafo_com_pesos
 import os
 from time import sleep
 
-def encontra_componente(componentes, vertice):
+def encontra_componente(componentes, vertice):  # Função que busca o componente ao qual um vértice pertence
     for componente, vertices in componentes.items():
         if vertice in vertices:
             return componente
@@ -30,17 +30,17 @@ def busca_gulosa_kruskal(grafo):
     arvore = set()
     componentes = {vertice: {vertice} for vertice in grafo}
 
-    for u, vizinhos in grafo.items():
-        for v, w in vizinhos:
+    for u, vizinhos in grafo.items():   # Percorre os vértices do grafo
+        for v, w in vizinhos:   # Percorre os vizinhos e pesos das arestas do vértice atual
             componente_u = encontra_componente(componentes, u)
             componente_v = encontra_componente(componentes, v)
 
-            if componente_u != componente_v:
-                arvore.add((u, v, w))
-                componentes[componente_u].update(componentes[componente_v])
+            if componente_u != componente_v:     # Se os componentes são diferentes, adiciona a aresta à árvore mínima de abrangência
+                arvore.add((u, v, w))   
+                componentes[componente_u].update(componentes[componente_v])  # Atualiza os componentes mesclando os conjuntos dos componentes de u e v
                 del componentes[componente_v]
 
-                if len(componentes) == 1:
-                    return arvore  # Pode encerrar assim que todos os vértices estiverem conectados
+                if len(componentes) == 1:   
+                    return arvore  # Encerra assim que todos os vértices estiverem conectados
 
-    return arvore
+    return arvore    # Retorna a árvore mínima de abrangência encontrada
